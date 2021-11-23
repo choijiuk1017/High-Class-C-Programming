9f53574db4fc4e765a1fd9fe1ab08372f1adb123
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2021-11-24 07:00:20 +0900

    고급 C 프로그래밍 중간고사

--- C177038 최지욱.c
+++ C177038 최지욱.c
@@ -1,176 +1,226 @@
 #include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+void problem1(void);
+void problem3(void);
+void problem4(void);
+void problem5(void);
+void problem6(void);
+int calculateCarParkingCharge(int Sdate, int Stime, int Sminute, int Edate, int Etime, int Eminute);
+int isLeapYear(int year);
+void displayTree(int pattern, int numberofLines);
+int save(int amount);
+const int saveLocalOnly(int amount);
+
 
 int main()
 {
+	problem1();
+	problem3();
+	problem4();
+	problem5();
+	problem6();
+}
 
-	int x, y, z, M, m, a, b, d, e, f, g;
-	int c = 0;
-	int sum = 0;
-	int f0 = 0;
-	int f1 = 1;
+void problem1()
+{
+	int Sdate =0, Stime=0, Sminute=0;
+	int Edate=0, Etime=0, Eminute=0;
+	int charge = 0;
 
-		
+	printf("주차 시작 시간을 입력하시오.(일, 시, 분): ");
+	scanf("%d, %d, %d", &Sdate, &Stime, &Sminute);
+
+	printf("주차 종료 시간을 입력하시오.(일, 시, 분): ");
+	scanf("%d, %d, %d", &Edate, &Etime, &Eminute);
+
+	charge = calculateCarParkingCharge(Sdate, Stime, Sminute, Edate, Etime, Eminute);
 	
-	printf("첫번째 문제\n");
-	printf("%d개의 정수를 입력하시오: ", 3);
-	scanf("%d %d %d", &x, &y, &z);
-	if (x >= y)
+	printf("주차 요금은 %d 입니다.", charge);
+
+	printf("\n");
+}
+
+void problem2()
+{
+
+
+}
+
+
+void problem3()
+{
+	int year;
+	int check;
+	printf("연도를 입력하시오: ");
+	scanf("%d", &year);
+	check = isLeapYear(year);
+	if (check == 1)
 	{
-		M = x;
-		m = y;
+		printf("윤년입니다.");
 	}
 	else
 	{
-		M = y;
-		m = x;
-	}
-	if (z > M)
-	{
-		M = z;
-	}
-	if (z < m)
-	{
-		m = z;
-	}
-	printf("제일 작은 정수는 %d입니다. \n\n", m);
-
-
-
-	printf("두번째 문제\n");
-	printf("좌표(x, y): ");
-	scanf("%d %d", &a, &b);
-	if (a > 0 && b > 0)
-	{
-		printf(" %d사분면\n\n", 1);
-	}
-	else if (a < 0 && b > 0)
-	{
-		printf(" %d사분면\n\n", 2);
-	}
-	else if (a < 0 && b < 0)
-	{
-		printf(" %d사분면\n\n", 3);
-	}
-	if (a > 0 && b < 0)
-	{
-		printf(" %d사분면\n\n", 4);
+		printf("윤년이 아닙니다.");
 	}
 
+	printf("\n");
+}
 
 
-	printf("세번째문제\n");
-	for (int i = 0; i < 100; i++)
+void problem4()
+{
+	int pattern;
+	int line;
+	printf("패턴을 입력하시오: ");
+	scanf("%d", &pattern);
+	printf("줄 수를 입력하시오: ");
+	scanf("%d", &line);
+	displayTree(pattern, line);
+
+
+	printf("\n");
+}
+
+void problem5()
+{
+	int money;
+	while (1)
 	{
-		if (i % 3 == 0)
+		printf("얼마를 입력하시겠습니까? (종료는 %d): ", -1);
+		scanf("%d", &money);
+		if (money == -1)
 		{
-			c += i;
-		}
-
-	}
-	printf("%d부터 %d 사이의 모든 %d의 배수의 합은 %d입니다. \n\n", 1, 100, 3, c);
-	
-
-
-	printf("네번째문제\n");
-	printf("정수를 입력하시오: ");
-	scanf("%d", &d);
-	printf("약수: ");
-	for (int i = 1; i <= d; i++)
-	{
-		if (d % i == 0)
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\n\n");
-
-
-
-	printf("다섯번째문제\n");
-	for (int i = 0; i < 8; i++)
-	{
-		for(int j = i; j < 8; j++)
-		{
-			printf(" ");
-		}
-		for(int k = i; k < i+i; k++)
-		{
-			printf("*");
-		}
-		printf("\n\n");
-	}
-	
-
-	printf("여섯번째문제\n");
-	printf("정수를 입력하시오: ");
-	scanf("%d", &e);
-	for (int i = 1; i <= e; i++)
-	{
-		for (int j = 1; j < i+1; j++)
-		{
-			printf("%d", j);
-		}
-		printf("\n\n");
-	}
-
-	
-	printf("일곱번째문제\n");
-	for (int i = 2; i <= 100; i++)
-	{
-		int j;
-
-		for (j = 2; j < i; j++)
-		{
-			if (i % j == 0)
-			{
-				break;
-			}
-		}
-		if (i == j)
-		{
-			printf("%d ", i);
-		}
-		
-	}
-	printf("\n\n");
-
-	printf("여덟번째문제\n");
-	for (int i = 0; i < 10000; i++)
-	{
-		sum = sum + i;
-		if (sum > 10000)
-		{
-			sum = sum - i;
-			i--;
+			printf("종료\n");
 			break;
-		}
-	}
-	printf("%d\n\n", sum);
-
-
-	printf("아홉번째문제\n");
-	printf("몇번째 항까지 구할까요? ");
-	scanf("%d", &f);
-	for (int i = 0; i <= f; i++)
-	{
-		if (i == 0)
-		{
-			g = f0;
-		}
-		else if (i == 1)
-		{
-			g = f1;
 		}
 		else
 		{
-			g = f0 + f1;
-			f0 = f1;
-			f1 = g;
+			printf("지금까지 총액은 %d입니다.\n", save(money));
 		}
-		
-		printf("%d ", g);
 	}
 
+
+	printf("\n");
 	
 }
 
+void problem6()
+{
+	int money;
+	while (1)
+	{
+		printf("얼마를 입력하시겠습니까? (종료는 %d): ", -1);
+		scanf("%d", &money);
+		if (money == -1)
+		{
+			printf("종료\n");
+			break;
+		}
+		else
+		{
+			printf("지금까지 총액은 %d입니다.\n", save(money));
+		}
+	}
+
+	printf("\n");
+
+}
+
+int calculateCarParkingCharge(int Sdate, int Stime, int Sminute, int Edate, int Etime, int Eminute)
+{
+	int sumtime = 0;
+	int checkdate = 0;
+	int charge = 0;
+
+	
+	sumtime =(60 * (24-(Stime - Etime)) + 60 - (Sminute - Eminute)) / 60 ;
+
+	checkdate = Edate - Sdate;
+
+	if (checkdate == 1)
+	{
+		if (sumtime <= 2)
+		{
+			charge = 3000;
+		}
+		else if(sumtime > 2 && sumtime < 24)
+		{
+			charge = (sumtime - 2) * 2000 + 3000;
+		}
+		else
+		{
+			charge = 6000;
+		}
+		return charge;
+	}
+	else
+	{
+		charge = checkdate * 6000;
+		return charge;
+	}
+
+	return 0;
+
+}
+
+int isLeapYear(int year)
+{
+	if ((year % 4 == 0) || (year % 100 != 0) && (year % 400 == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void displayTree(int pattern, int numberofLines)
+{
+	if (pattern == 1)
+	{
+		for (int i = 0; i <= numberofLines; i++)
+		{
+			for (int j = numberofLines ; j >= i - 1; j--)
+			{
+				printf(" ");
+			}
+			for (int j = 0; j < 2 * i - 1; j++)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+	
+	}
+	else if(pattern == 2)
+	{
+		for (int i = 0; i <= numberofLines; i++)
+		{
+			for (int j = 0; j < i ; j++)
+			{
+				printf(" ");
+			}
+			for (int j = numberofLines +1 ; j >= 2*i -1; j--)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+}
+
+int save(int amount)
+{
+	static int sum = 0;
+	sum = sum + amount;
+	return sum;
+}
+
+const int saveLocalOnly( int amount)
+{
+	int sum = 0;
+	sum = sum + amount;
+	return sum;
+}
