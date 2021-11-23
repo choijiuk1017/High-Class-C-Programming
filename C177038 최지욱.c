ed84c823f11b5965216214dd8602cb73dab5f49f
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2021-11-24 07:02:41 +0900

    고급 C프로그래밍 포인터 과제

--- C177038 최지욱.c
+++ C177038 최지욱.c
@@ -1,39 +1,21 @@
 #include<stdio.h>
-
-void transpose(int a[][3], int b[][3]);
+void pointer(int* A);
 
 int main()
 {
-	int a[3][3] = {
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}
-	};
+	int A[] = { 1, 2, 3, 4, 5 };
 
-	int b[3][3];
-
-	transpose(a, b);
+	pointer(&A);
 
 	return 0;
 }
 
-void transpose(int a[][3], int b[][3])
+void pointer(int* A)
 {
-	for (int i = 0; i < 3; i++)
+	printf("A[] = {");
+	for (int i = 0 ; i < 5; i++)
 	{
-		for (int j = 0; j < 3; j++)
-		{
-			b[i][j] = a[j][i];
-		}
+		printf(" %d ", A[i]);
 	}
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%d ", b[i][j]);
-		}
-		printf("\n");
-	}
-
+	printf("}");
 }
